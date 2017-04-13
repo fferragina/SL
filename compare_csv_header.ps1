c317dd17eaec4fc9325374ba5e713d86dd13fd9e
Franco Ferragina <francoferragina@FMBP2.local>
2017-04-13 10:33:42 +0200

    ok

---
+++ compare_csv_header.ps1
@@ -0,0 +1,22 @@
+﻿$path1 = "C:\Users\SL_HSR_1\Dropbox\project\SL\DH_OEV_20170114\"
+$path2 = "C:\Users\SL_HSR_1\Dropbox\project\SL\DH_OEV_20170128\"
+
+$files = Get-ChildItem -Path $Path1 -filter *.csv | Sort-Object Name
+
+ForEach ($file in $files){
+    
+    Write-Host $file
+    $file1 = $path1+$file
+    $file2 = $path2+$file
+        
+    $header1 = (Get-Content $file1 | select -First 1) -split "\|"
+    $header2 = (Get-Content $file2 | select -First 1) -split "\|"
+
+    (Get-Content $file1 | select -First 2) -split "\|" | out-file $file1+"_"
+    (Get-Content $file2 | select -First 2) -split "\|" | out-file $file2+"_"
+    
+    Write-Host $header1
+    Write-Host $header2
+    
+    Compare-Object $header2 $header1 | Select-Object @{Name='File';Expression={$file}},InputObject,SideIndicator 
+}
---
+++ import.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module .\CsvSqlimport.psm1
+Import-CsvToSql -Csv .\DH_OEV_20170114\VERTRAG14.csv -SqlServer HSR-7 -Database import -Delimiter "|" 
+Import-CsvToSql -Csv .\DH_OEV_20170114\VERTRAGSTEIL14.csv -SqlServer HSR-7 -Database import -Delimiter "|" 
+Import-CsvToSql -Csv .\DH_OEV_20170128\VERTRAG28.csv -SqlServer HSR-7 -Database import -Delimiter "|" 
+Import-CsvToSql -Csv .\DH_OEV_20170128\VERTRAGSTEIL28.csv -SqlServer HSR-7 -Database import -Delimiter "|" 
